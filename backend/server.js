ee296e69cab4aadb882c1b6f372f894f2ccc9d93
ranishstha <ranishstha@gmail.com>
2024-09-09 21:13:15 -0400

    JWT, password encrypt decrypt, error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const cors = require('cors');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express()
 // Use CORS middleware
@@ -17,19 +19,16 @@ app.use((req, res, next) => {
 dotenv.config()
 connectDB()
 
+app.use(express.json())  // To accept JSON data
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-})
+app.use('/api/user', userRoutes)
 
-app.get('/api/chat/:id', (req, res) => {
-    // console.log(req.params.id)
-    const singleChat = chats.find((x) => x._id === req.params.id);
-    res.send(singleChat)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT;
